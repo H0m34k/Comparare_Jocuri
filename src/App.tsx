3252fe332eb73448822fb18dac93e48b55bb1986
Lena434 <lenalozovanu@gmail.com>
2026-02-08 23:02:22 +0200

    Add ComparePage with game selection and preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
-import GameListPage from "./pages/GameListPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import GameListPage from './pages/GameListPage';
+import ComparePage from './pages/ComparePage';  // ← ACEST IMPORT
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/games" element={<GameListPage />} />
+          <Route path="/compare" element={<ComparePage />} />  {/* ← ACEST ROUTE */}
         </Routes>
       </MainLayout>
     </BrowserRouter>
